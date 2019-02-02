f146c84293201e18f66737da7891cf50cae1e396
mmyenny <mmy491@gmail.com>
2019-02-02 01:04:28 -0500

    Pulling quote dynamically from static state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      quote:
+        'When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.'
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Quote of the Day</h1>
-        <p>
-          "When one door of happiness closes, another opens, but often we look
-          so long at the closed door that we do not see the one that has been
-          opened for us."
-        </p>
+        <p>{this.state.quote}</p>
       </div>
     )
   }
