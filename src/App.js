69191fd3aff6f6c873c38212651187fa48307e08
mmyenny <mmy491@gmail.com>
2019-02-01 23:39:49 -0500

    Initial react app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Quote of the Day</h1>
+        <p>
+          "When one door of happiness closes, another opens, but often we look
+          so long at the closed door that we do not see the one that has been
+          opened for us."
+        </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
